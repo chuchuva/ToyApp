4cdb62d03202a2cc149a467dcd8a1f0f80e0d755
Pavel Chuchuva <Pavel.Chuchuva@software.dell.com>
2015-09-11 15:48:05 +1000

    :boom: Initial commit

---
+++ ToyApp/Controllers/StaticPagesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToyApp.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        // Home
+        // Help
+        // About
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ ToyApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToyApp.MvcApplication" Language="C#" %>
