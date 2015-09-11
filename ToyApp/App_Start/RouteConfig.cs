592b103ac81bb81d11844b569ffcb04185d36b3e
Pavel Chuchuva <Pavel.Chuchuva@software.dell.com>
2015-09-11 15:58:27 +1000

    Fix home page

--- ToyApp/App_Start/RouteConfig.cs
+++ ToyApp/App_Start/RouteConfig.cs
@@ -13,13 +13,14 @@ namespace ToyApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "StaticPages", action = "Home", id = UrlParameter.Optional }
             );
-
-            
+        
         }
     }
 }
