41a3d1b7b4788121b8ce08070c2f6feda251a276
Pavel Chuchuva <Pavel.Chuchuva@software.dell.com>
2015-11-06 16:42:38 +1100

    Add movies

---
+++ ToyApp/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToyApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string PlotOutline { get; set; }
+    }
+}
